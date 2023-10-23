374da0d453db08510ad01e8d58f12e49dbb9fe8a
jstet <mail@jstet.net>
2023-10-23 16:14:51 -0700

    add quality ensurance tools

---
+++ src/routes/+page.server.js
@@ -0,0 +1,13 @@
+import directus_fetch from '$lib/js/directus_fetch';
+import {indexQuery} from './query.js';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({params}) {
+  const data = await directus_fetch(indexQuery, {});
+
+  const organizations = data.Organizations
+
+  return {
+    Organizations: organizations
+  };
+}
---
+++ src/routes/query.js
@@ -0,0 +1,8 @@
+export const indexQuery = `
+query Index (
+	
+) {
+	
+	}
+}
+`;
