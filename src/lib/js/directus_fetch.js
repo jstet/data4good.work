e73b200629bd9532f6c00ac0b4084c4a96f1df37
jstet <mail@jstet.net>
2023-11-23 13:34:25 -0800

    fix weird close #19

--- src/lib/js/directus_fetch.js
+++ src/lib/js/directus_fetch.js
@@ -3,7 +3,9 @@ import {PUBLIC_API_URL} from '$env/static/public';
 
 const client = createDirectus(PUBLIC_API_URL).with(graphql());
 
-export default async function directus_fetch(query) {
+export async function directus_fetch(query) {
   const result = await client.query(query);
   return result;
 }
+
+export default directus_fetch;
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,8 +2,8 @@ import directus_fetch from '$lib/js/directus_fetch';
 import {indexQuery} from './query.js';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({}) {
-  const data = await directus_fetch(indexQuery);
+export function load({}) {
+  const data = directus_fetch(indexQuery, {});
 
   return {
     Organizations: data.Organizations,
